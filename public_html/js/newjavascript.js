43f73a34b673b6ddbf3f865a4db78697a589c3f7
CesarBrianoAviles <98993667+CesarBrianoAviles@users.noreply.github.com>
2022-03-21 21:27:40 -0600

    Add files via upload

---
+++ public_html/js/newjavascript.js
@@ -0,0 +1,30 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+       
+
+function cambiar(){
+    document.getElementById("titulo").innerHTML ="imagen1";
+                            
+}
+    document.getElementById("boton1").onclick = function(){
+    cambiar();
+    
+}
+  
+function cambiar1(){
+    document.getElementById("titulo").innerHTML ="imagen2";
+                            
+}
+    document.getElementById("boton2").onclick = function(){
+    cambiar1();
+    
+}  
+
+
+
+
+
+
